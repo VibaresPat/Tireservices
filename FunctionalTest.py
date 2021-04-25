3a55da6fc97b9dd4d269a1873a403c85bec9d429
Patricia <patricia>
2021-04-26 05:28:26 +0800

    add

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,129 +1,163 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http:/127.0.0.1:8000')
-
-
-
-
-
-
-'''
 import unittest
 
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 class PageTest(unittest.TestCase):
 
+	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-	
+		
+
 	def test_browser_title(self):
 		self.browser.get('http://localhost:8000')
-		self.assertIn('Baybayin Project', self.browser.title)
+		self.assertIn('Request Form', self.browser.title)
 
-		#H1
-		headerText = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Welcome to Baybayin Project', headerText)
-		time.sleep(2)
+		Vext = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Request Form', Vext)
 
-		#Username
-		input1 = self.browser.find_element_by_tag_name('p').text
-		self.assertIn('USERNAME:', input1)
-		firstname = self.browser.find_element_by_id('firstname').send_keys("Patricia")
-		time.sleep(2)
+		Hext = self.browser.find_element_by_tag_name('h2').text
+		self.assertIn('-ICT LESSON-', Hext)
 
-		input2 = self.browser.find_element_by_id('lesson').text
-		self.assertIn('LECTURE:', input2)
+		p = self.browser.find_element_by_id('u').text
+		self.assertIn('FULLNAME:', p)
 
-		#lecture
-		select = self.browser.find_element_by_id('quantity').send_keys("1")
-		time.sleep(2)
+		a = self.browser.find_element_by_id('n').text
+		self.assertIn('EMAIL:', a)
 
-		#askme
-		input3 = self.browser.find_element_by_id('help').text
-		self.assertIn('QUESTION:', input3)
-		Baybayin = self.browser.find_element_by_id('baybayin').send_keys("Askme")
-		time.sleep(2)
-		firstname = self.browser.find_element_by_id('done').click()
+		t = self.browser.find_element_by_id('a').text
+		self.assertIn('DATE:', t)
+
+		p2 = self.browser.find_element_by_id('pangalawa').text
+		self.assertIn('COURSE AND SECTION:', p2)
+
+		p3 = self.browser.find_element_by_id('pangatlo').text
+		self.assertIn('LESSON:', p3)
+
+		p4 = self.browser.find_element_by_id('pangapat').text
+		self.assertIn('REASON:', p4)
+
+		p5 = self.browser.find_element_by_id('panglima').text
+		self.assertIn('Note: Provide a formal letter for requesting.', p5)
+
+		form = self.browser.find_element_by_tag_name('form')
+
+		lesson0 = self.browser.find_element_by_name('lesson').click()
+		lesson1 = self.browser.find_element_by_name('lesson').click()
+		lesson2 = self.browser.find_element_by_name('lesson').click()
+
+		Select1 = self.browser.find_element_by_id('lec').click()
+		Select2 = self.browser.find_element_by_id('lab').click()
+		Select3 = self.browser.find_element_by_id('all').click()
+	
+
+		i= self.browser.find_element_by_id('firstname')  
+		self.assertEqual(i.get_attribute('placeholder'),'SN/FN/MN')
+		i = self.browser.find_element_by_id('firstname').send_keys("Nice")
+		time.sleep(1)
+
+		n= self.browser.find_element_by_id('Gmail')  
+		self.assertEqual(n.get_attribute('placeholder'),'@gmail')
+		n= self.browser.find_element_by_id('Gmail').send_keys("Nice")
+		time.sleep(1)
+
+		u= self.browser.find_element_by_id('birthday') 
+		time.sleep(1)
+
+		l= self.browser.find_element_by_id('coursesection')  
+		self.assertEqual(l.get_attribute('placeholder'),'Ex: BSIE-ICT-3A')
+		l= self.browser.find_element_by_id('coursesection').send_keys("Nice")
+		time.sleep(1)
+
+		k= self.browser.find_element_by_id('lec')  
+		w= self.browser.find_element_by_id('lab') 
+		z= self.browser.find_element_by_id('all') 
+		time.sleep(1)
+
+		f= self.browser.find_element_by_id('letter')  
+		self.assertEqual(f.get_attribute('placeholder'),'Valid Reason')
+		f= self.browser.find_element_by_id('letter').send_keys("Nice")
+		time.sleep(1)
+
+		r= self.browser.find_element_by_id('done') 
+		time.sleep(1)
+
+		dat = self.browser.find_element_by_id('birthday').click()
+		dat = self.browser.find_element_by_id('birthday').send_keys("2012-11-02")
+		time.sleep(1)
+
+		sub = self.browser.find_element_by_id('done').click()
+		time.sleep(1)
 
 
-if __name__== '__main__':
-	unittest.main()	
+		self.browser.quit()	
+		self.browser = webdriver.Firefox()
+		self.browser.get('http://localhost:8000')
 
-	browser = webdriver.Firefox()
-	browser.get('http://127.0.0.1:8000')
+		lesson0 = self.browser.find_element_by_name('lesson').click()
+		lesson1 = self.browser.find_element_by_name('lesson').click()
+		lesson2 = self.browser.find_element_by_name('lesson').click()
 
-		#H1
-		headerText = self.browser.find_element_by_tag_name('h1').text
-		self.browser.get('Welcome to Baybayin Project', headerText)
+		Select1 = self.browser.find_element_by_id('lec').click()
+		Select2 = self.browser.find_element_by_id('lab').click()
+		Select3 = self.browser.find_element_by_id('all').click()
+	
 
-		#Username
-		input1 = self.browser.find_element_by_tag_name('p').text
-		self.assertIn('USER NAME:', input1)
-		firstname = self.browser.find_element_by_id('firstname').send_keys("Patricia")
+		i= self.browser.find_element_by_id('firstname')  
+		self.assertEqual(i.get_attribute('placeholder'),'SN/FN/MN')
+		i = self.browser.find_element_by_id('firstname').send_keys("Nice")
+		time.sleep(1)
 
-		#lecture
-		input2 = self.browser.find_element_by_tag_name('p').send_keys("1")
+		n= self.browser.find_element_by_id('Gmail')  
+		self.assertEqual(n.get_attribute('placeholder'),'@gmail')
+		n= self.browser.find_element_by_id('Gmail').send_keys("Nice")
+		time.sleep(1)
 
-		#askme
-		input3 = self.browser.find_element_by_tag_name('p').text
-		self.assertIn('QUESTION:', input3)
-		Baybayin = self.browser.find_element_by_id('baybayin').send_keys("Ask me")
+		u= self.browser.find_element_by_id('birthday') 
+		time.sleep(1)
+
+		l= self.browser.find_element_by_id('coursesection')  
+		self.assertEqual(l.get_attribute('placeholder'),'Ex: BSIE-ICT-3A')
+		l= self.browser.find_element_by_id('coursesection').send_keys("Nice")
+		time.sleep(1)
+
+		k= self.browser.find_element_by_id('lec')  
+		w= self.browser.find_element_by_id('lab') 
+		z= self.browser.find_element_by_id('all') 
+		time.sleep(1)
+
+		f= self.browser.find_element_by_id('letter')  
+		self.assertEqual(f.get_attribute('placeholder'),'Valid Reason')
+		f= self.browser.find_element_by_id('letter').send_keys("Nice")
+		time.sleep(1)
+
+		r= self.browser.find_element_by_id('done') 
+		time.sleep(1)
+
+		dat = self.browser.find_element_by_id('birthday').click()
+		dat = self.browser.find_element_by_id('birthday').send_keys("2012-11-02")
+		time.sleep(1)
+
+		sub = self.browser.find_element_by_id('done').click()
+		time.sleep(1)
+
+
+		self.browser.quit()	
 		
-		firstname = self.browser.find_element_by_tag_name('done').click()
 
-if __name__== '__main__':
+
+		
+
+
+
+		
+	
+
+
+		
+
+
+if __name__ == '__main__':
 	unittest.main()
-		
-		#Username
-		input1 = self.browser.find_element_by_tag_id('p').text
-		self.assertIn('USERNAME:', input1)
-		firstname = self.browser.find_element_by_tag_name('firstname').send_keys("Patricia")
-		
-		#lecture
-		input2 = self.browser.find_element_by_id('lesson').text
-		self.assertIn('LECTURE:', input2)
-		input2 = self.browser.find_element_by_tag_name('h5').send_keys("1")
-
-		#askme
-		input3 = self.browser.find_element_by_id('h6').text
-		self.assertIn('QUESTION:', input3)
-		Baybayin = self.browser.find_element_by_tag_name('baybayin').send_keys("Ask me")
-		
-		firstname = self.browser.find_element_by_tag_name('done').click()
-
-
-if __name__== '__main__':
-	unittest.main()
-
-		#lecture
-		input2 = self.browser.find_element_by_tag_name('lesson').text
-		self.assertIn('LECTURE:', input2)
-
-		input4 = self.browser.find_element_by_tag_name('h5').send_keys("1")
-
-		#askme
-		input3 = self.browser.find_element_by_id('h6').text
-		self.assertIn('QUESTION:', input3)
-		Baybayin = self.browser.find_element_by_tag_name('baybayin').send_keys("Ask me")
-		
-		firstname = self.browser.find_element_by_tag_name('done').click()
-
-
-if __name__== '__main__':
-	unittest.main()
-
-		#askme
-		input3 = self.browser.find_element_by_tag_name('p').text
-		self.assertIn('QUESTION:', input3)
-		Baybayin = self.browser.find_element_by_tag_name('baybayin').send_keys("Ask me")
-		
-		firstname = self.browser.find_element_by_id('done').click()
-
-
-if __name__== '__main__':
-	unittest.main()	'''
-
-
